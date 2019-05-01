3be4affe13251d9927d15e136fe2a402f2d9decb
Gaetan DURON <gduron@e3r7p5.42.fr>
2019-05-01 14:33:40 +0200

    create a mmap wrapper

--- srcs/libft_malloc.c
+++ srcs/libft_malloc.c
@@ -1,6 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft_malloc.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gduron <gduron@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/01 13:30:22 by gduron            #+#    #+#             */
+/*   Updated: 2019/05/01 14:31:43 by gduron           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft_malloc.h"
 
-void *malloc(size_t len)
+void		*ft_mmap(size_t size)
 {
-	return mmap(0, len, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+	size_t n;
+	size_t pagesize;
+
+	pagesize = getpagesize();
+	n = (size / pagesize) - (size % pagesize == 0) + 1;
+	return (mmap(0, n, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0));
 }
