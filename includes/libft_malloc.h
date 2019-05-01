f780745fe08dd3d36f374ee513fd5bfb83166b02
Gaetan DURON <gaetan.duron@gmail.com>
2019-05-01 16:21:24 +0200

    wip set bin header

--- includes/libft_malloc.h
+++ includes/libft_malloc.h
@@ -1,9 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft_malloc.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gduron <gduron@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/01 15:20:29 by gduron            #+#    #+#             */
+/*   Updated: 2019/05/01 15:47:16 by gduron           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MALLOC_H
 # define MALLOC_H
 
 # include <sys/mman.h>
 # include <unistd.h>
 
+typedef struct		s_bin
+{
+	struct s_bin	*last;
+	struct s_bin	*next;
+}					t_bin;
+
+typedef enum        e_zone
+{
+    TINY,
+    SMALL,
+    LARGE,
+    MAX_ZONE
+}                   t_zone;
+
 void	*malloc(size_t size);
 void    *ft_mmap(size_t size);
+void	*set_bin_headers(void *memory, size_t size, t_bin *large_zone_head);
 #endif
--- srcs/libft_malloc.c
+++ srcs/libft_malloc.c
@@ -6,7 +6,7 @@
 /*   By: gduron <gduron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/01 13:30:22 by gduron            #+#    #+#             */
-/*   Updated: 2019/05/01 14:55:09 by gduron           ###   ########.fr       */
+/*   Updated: 2019/05/01 15:47:07 by gduron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,13 @@ void		*ft_mmap(size_t size)
 	pagesize = getpagesize();
 	n = pagesize * ((size / pagesize) - (size % pagesize == 0) + 1);
 	return (mmap(0, n, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0));
+}
+
+void	*set_bin_headers(void *memory, size_t size, t_bin *large_zone_head)
+{
+	t_bin new_bin;
+
+	new_bin.last = 0;
+	new_bin.next = large_zone_head;
+	((t_bin*)memory)[0] = new_bin;
 }
