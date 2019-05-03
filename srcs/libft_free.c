29854129ef82ed5e473e51571454d443a61ebe80
Gaetan DURON <gaetan.duron@gmail.com>
2019-05-03 17:08:51 +0200

    +1 to reach the next chunk

--- srcs/libft_free.c
+++ srcs/libft_free.c
@@ -6,7 +6,7 @@
 /*   By: gduron <gduron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/03 13:00:13 by gduron            #+#    #+#             */
-/*   Updated: 2019/05/03 14:45:11 by gduron           ###   ########.fr       */
+/*   Updated: 2019/05/03 16:59:25 by gduron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ int	ft_free(void *ptr)
 	if (ptr == 0)
 		return (2);
 	chunk = (t_chunk*)ptr - 1;
-	if (!(chunk[(chunk->size / 16)].size & 0b1))
+	if ((chunk[(chunk->size / 16) + 1].size & 0b1) == 0)
 		return (0);
 	if (chunk->size > MAX_SMALL_CHUNK)
 		return (free_bin(ptr, chunk->size));
