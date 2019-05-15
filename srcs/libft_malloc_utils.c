3033d129b78b5ab1a352d2774ccff51e36f9ed43
Gaetan DURON <gaetan.duron@gmail.com>
2019-05-15 18:50:48 +0200

    Fix empty bin set as used

--- srcs/libft_malloc_utils.c
+++ srcs/libft_malloc_utils.c
@@ -6,7 +6,7 @@
 /*   By: gduron <gduron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/05 13:51:29 by gduron            #+#    #+#             */
-/*   Updated: 2019/05/05 20:15:56 by gduron           ###   ########.fr       */
+/*   Updated: 2019/05/15 18:45:10 by gduron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ void	*search_in_zone(size_t size, int zone)
 		chunk = bin_get_first_chunk(bin);
 		while ((chunk->size & LAST_CHUNK_FLAG) == 0)
 		{
-			if (chunk->size >= size)
+			if (chunk->size >= size && get_next_chunk(chunk)->size % 2 == 0)
 				return (set_chunk_header(chunk, size, zone));
 			chunk = get_next_chunk(chunk);
 		}
@@ -65,11 +65,14 @@ int		create_bin(int zone)
 {
 	t_bin	*bin;
 	int		bin_size[2];
+	t_chunk	*last_chunk;
 
 	bin_size[0] = (MAX_TINY_CHUNK + 8) * 100;
 	bin_size[1] = (MAX_SMALL_CHUNK + 8) * 100;
 	bin = (t_bin*)((size_t*)ft_malloc(bin_size[zone]) - 3);
 	remove_bin_from_zone(bin, LARGE);
 	add_bin_to_zone(bin, zone);
+	last_chunk = get_next_chunk(bin_get_first_chunk(bin));
+	last_chunk->size = LAST_CHUNK_FLAG;
 	return (0);
 }
